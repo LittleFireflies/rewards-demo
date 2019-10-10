886ed43a57425909ec2459c86f08bc1da4ccc2e2
Widyarso Joko Purnomo <widyjp10@gmail.com>
2019-10-10 19:31:10 +0700

    fix /rewards json response

--- backend/routes/rewards.js
+++ backend/routes/rewards.js
@@ -2,7 +2,14 @@ const express = require('express')
 const router = express.Router()
 
 router.get("/rewards", (req, res) => {
-    res.send("Rewards")
+    var rewards1 = {"name": "HP Pavilion Power 580-142D Desktop PC Gaming with HP 23ES Monitor [23 Inch]", "points": 50000, "available": true}
+    var rewards2 = {"name": "Nintendo Switch Game Console", "points": 15000, "available": true}
+    var rewards3 = {"name": "Samsung Galaxy A50 Smartphone", "points": 11000, "available": true}
+    var rewards4 = {"name": "Samsung Curved Monitor 24 inch", "points": 7000, "available": true}
+    var rewards5 = {"name": "Jaket Hoodie Dicoding", "points": 1500, "available": true}
+    var rewards6 = {"name": "T-shirt Dicoding Graduate", "points": 300, "available": true}
+
+    res.json([rewards1, rewards2, rewards3, rewards4, rewards5, rewards6])
 })
 
 module.exports = router
